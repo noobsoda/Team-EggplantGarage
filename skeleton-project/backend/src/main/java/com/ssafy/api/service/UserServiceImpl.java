f6939205f72a87d02e69133d486ea2cd1f6a813d
yujaegyun <sodamito3@gmail.com>
2023-01-18 17:36:44 +0900

    Feat : DB Entity 변경 및 Auditing 적용

--- skeleton-project/backend/src/main/java/com/ssafy/api/service/UserServiceImpl.java
+++ skeleton-project/backend/src/main/java/com/ssafy/api/service/UserServiceImpl.java
@@ -26,23 +26,22 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public User createUser(UserRegisterPostReq userRegisterInfo) {
 		User user = new User();
-		user.setUserId(userRegisterInfo.getId());
+		user.setUserId(userRegisterInfo.getUserId());
 		// 보안을 위해서 유저 패스워드 암호화 하여 디비에 저장.
-		user.setPassword(passwordEncoder.encode(userRegisterInfo.getPassword()));
+		user.setUserPassword(passwordEncoder.encode(userRegisterInfo.getUserPassword()));
 		return userRepository.save(user);
 	}
 
 	@Override
 	public User getUserByUserId(String userId) {
 		// 디비에 유저 정보 조회 (userId 를 통한 조회).
-		User user = userRepositorySupport.findUserByUserId(userId).get();
+		User user = userRepository.findByUserId(userId).get();
 		return user;
 	}
 
 	@Override
 	public User deleteUser(String userId) {
-		User user = userRepositorySupport.findUserByUserId(userId).get();
-		System.out.println(user);
+		User user = userRepository.findByUserId(userId).get();
 		userRepository.delete(user);
 		return null;
 	}
--- skeleton-project/backend/src/main/java/com/ssafy/db/entity/User.java
+++ skeleton-project/backend/src/main/java/com/ssafy/db/entity/User.java
@@ -5,8 +5,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 /**
  * 유저 모델 정의.
@@ -14,13 +17,23 @@ import javax.persistence.Entity;
 @Entity
 @Getter
 @Setter
-public class User extends BaseEntity{
-    String position;
-    String department;
-    String name;
-    String userId;
+@EntityListeners(AuditingEntityListener.class)
+public class User{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userNo = null;
+    @Column(unique = true)
+    private String userId;
+    private String userName;
+    private String userNickname;
+    private String phone;
+    private String bank;
+    private String account;
+    @CreatedDate
+    private LocalDateTime createAt;
+    private String refreshToken;
 
     @JsonIgnore
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    String password;
+    private String userPassword;
 }
