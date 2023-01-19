fc00f2529adf648236dc633d0898d74ab47257cb
yujaegyun <sodamito3@gmail.com>
2023-01-19 17:29:03 +0900

    Feat : 통합빌드 Test 리액트 연걸 성공

--- skeleton-project/backend/src/main/java/com/ssafy/config/WebMvcConfig.java
+++ skeleton-project/backend/src/main/java/com/ssafy/config/WebMvcConfig.java
@@ -52,16 +52,17 @@ public class WebMvcConfig implements WebMvcConfigurer {
     		 * Front-end에서 참조하는 URL을 /dist로 매핑
     		 * 
     		 */
-        registry.addResourceHandler("/css/**")
-        			.addResourceLocations("classpath:/dist/css/");
-        	registry.addResourceHandler("/fonts/**")
-        			.addResourceLocations("classpath:/dist/fonts/");
-        registry.addResourceHandler("/icons/**")
-				.addResourceLocations("classpath:/dist/icons/");
-        registry.addResourceHandler("/img/**")
-			.addResourceLocations("classpath:/dist/img/");
-        registry.addResourceHandler("/js/**")
-				.addResourceLocations("classpath:/dist/js/");
+        registry.addResourceHandler("/static/css/**")
+        			.addResourceLocations("classpath:/dist/static/css/");
+        	registry.addResourceHandler("/static/fonts/**")
+        			.addResourceLocations("classpath:/dist/static/fonts/");
+        registry.addResourceHandler("/static/icons/**")
+				.addResourceLocations("classpath:/dist/static/icons/");
+        registry.addResourceHandler("/static/img/**")
+			.addResourceLocations("classpath:/dist/static/img/");
+        registry.addResourceHandler("/static/js/**")
+				.addResourceLocations("classpath:/dist/static/js/");
+        
     }
 
     public Filter requestLoggingFilter() {
---
+++ skeleton-project/frontend/src/Components/Pages/Test.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Test() {
+  return (
+    <header className="App-header">
+      <img src="logo512.png" className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  );
+}
