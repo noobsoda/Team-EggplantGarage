142be83390b4995b4b02bbdafbb94eedde0d190e
yujaegyun <sodamito3@gmail.com>
2023-01-19 14:45:50 +0900

    Fix : Bean 생성자 주입으로 변경

--- skeleton-project/backend/src/main/java/com/ssafy/api/controller/AuthController.java
+++ skeleton-project/backend/src/main/java/com/ssafy/api/controller/AuthController.java
@@ -28,12 +28,16 @@ import io.swagger.annotations.ApiResponse;
 @RestController
 @RequestMapping("/api/v1/auth")
 public class AuthController {
+
+	private final UserService userService;
+	private final PasswordEncoder passwordEncoder;
 	@Autowired
-	UserService userService;
-	
-	@Autowired
-	PasswordEncoder passwordEncoder;
-	
+	public AuthController(UserService userService, PasswordEncoder passwordEncoder){
+		this.userService = userService;
+		this.passwordEncoder = passwordEncoder;
+	}
+
+
 	@PostMapping("/login")
 	@ApiOperation(value = "로그인", notes = "<strong>아이디와 패스워드</strong>를 통해 로그인 한다.") 
     @ApiResponses({
@@ -43,14 +47,14 @@ public class AuthController {
         @ApiResponse(code = 500, message = "서버 오류", response = BaseResponseBody.class)
     })
 	public ResponseEntity<UserLoginPostRes> login(@RequestBody @ApiParam(value="로그인 정보", required = true) UserLoginPostReq loginInfo) {
-		String userId = loginInfo.getUserId();
+		String userEmail = loginInfo.getUserEmail();
 		String password = loginInfo.getUserPassword();
 		
-		User user = userService.getUserByUserId(userId);
+		User user = userService.getUserByUserEmail(userEmail);
 		// 로그인 요청한 유저로부터 입력된 패스워드 와 디비에 저장된 유저의 암호화된 패스워드가 같은지 확인.(유효한 패스워드인지 여부 확인)
 		if(passwordEncoder.matches(password, user.getUserPassword())) {
 			// 유효한 패스워드가 맞는 경우, 로그인 성공으로 응답.(액세스 토큰을 포함하여 응답값 전달)
-			return ResponseEntity.ok(UserLoginPostRes.of(200, "Success", JwtTokenUtil.getToken(userId)));
+			return ResponseEntity.ok(UserLoginPostRes.of(200, "Success", JwtTokenUtil.getToken(userEmail)));
 		}
 		// 유효하지 않는 패스워드인 경우, 로그인 실패로 응답.
 		return ResponseEntity.status(401).body(UserLoginPostRes.of(401, "Invalid Password", null));
