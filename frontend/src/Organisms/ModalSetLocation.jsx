51f53151df254b42659a9ff043811417f54745bf
graywhale <bomerj@naver.com>
2023-01-31 13:51:10 +0900

    [Feat] Map API 지도 현재위치와 지역설정 적용

--- frontend/src/Organisms/ModalSetLocation.jsx
+++ frontend/src/Organisms/ModalSetLocation.jsx
@@ -3,9 +3,7 @@ import styled from "styled-components";
 import MediumBtn from "../Atoms/Buttons/MediumBtn";
 import Modal from "../Atoms/Modal";
 import ModalBody from "../Templates/Modal/ModalBody";
-import { useEffect } from "react";
-
-const { kakao } = window;
+import MapContainer from "../Templates/Modal/MapContainer";
 
 const Header = styled.div`
   width: 360px;
@@ -28,20 +26,7 @@ export default function ModalSetLocation({ setModalOpen }) {
   const apply = () => {
     closeModal();
   };
-  const Map = () => {
-    useEffect(() => {
-      let container = document.getElementById("map");
 
-      let options = {
-        center: new window.kakao.maps.LatLng(35.85133, 127.734086),
-        level: 13,
-      };
-
-      let map = new window.kakao.maps.Map(container, options);
-
-      console.log("loading kakaomap");
-    }, []);
-  };
   return (
     <Modal>
       <div height="24px" className="page-header">
@@ -54,9 +39,7 @@ export default function ModalSetLocation({ setModalOpen }) {
         </div>
       </Header>
       <ModalBody>
-        <div width="280px" height="280px" id="map">
-          야호
-        </div>
+        <MapContainer />
         <MediumBtn name="초기화" />
         <MediumBtn name="적용" buttonClick={apply} />
       </ModalBody>
