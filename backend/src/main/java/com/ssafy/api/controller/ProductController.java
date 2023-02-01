21f39e4359f8d8cee3c700971eea1a7e048a194c
yujaegyun <sodamito3@gmail.com>
2023-02-01 17:39:35 +0900

    [Feat] : Product 컨트롤러 및 Req 스켈레톤 추가

--- backend/src/main/java/com/ssafy/api/controller/ProductController.java
+++ backend/src/main/java/com/ssafy/api/controller/ProductController.java
@@ -1,2 +1,48 @@
-package com.ssafy.api.controller;public class ProductController {
+package com.ssafy.api.controller;
+
+import com.ssafy.api.request.ProductsRegisterPostReq;
+import com.ssafy.api.service.UserService;
+import com.ssafy.common.model.response.BaseResponseBody;
+import io.swagger.annotations.*;
+import org.slf4j.Logger;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@Api(value = "상품 API", tags = {"Product"})
+@RestController
+@RequestMapping("/api/v1/products")
+public class ProductController {
+    private final Logger logger;
+    private final UserService userService;
+
+    public ProductController(Logger logger, UserService userService) {
+        this.logger = logger;
+        this.userService = userService;
+    }
+
+    @PostMapping("/{id}")
+    @ApiOperation(value = "상품 등록", notes = "현재 라이브에 상품을 등록한다..")
+    @ApiResponses({@ApiResponse(code = 201, message = "Created"), @ApiResponse(code = 401, message = "만료됨"), @ApiResponse(code = 403, message = "인증 실패"), @ApiResponse(code = 500, message = "서버 오류")})
+    public ResponseEntity<? extends BaseResponseBody> postProductCreate(@RequestParam @ApiParam(value = "방 생성 정보", required = true) ProductsRegisterPostReq productRegisterPostReq,
+                                                                        @RequestParam MultipartFile img, @PathVariable("id") String id) {
+
+        //유저 확인
+
+        /*if (user == null)
+            return ResponseEntity.status(401).body(BaseResponseBody.of(401, "세션이 만료된 사용자입니다."));
+        //url 중복 체크
+        if (liveService.getLiveCheckUrlByUrl(liveRegisterInfo.getUrl())) {
+            return ResponseEntity.status(409).body(BaseResponseBody.of(409, "방송 url이 중복됩니다"));
+        }
+        //db에 저장 및 생성
+        else {
+            liveService.CreateLive(liveRegisterInfo, user);
+            return ResponseEntity.status(201).body(BaseResponseBody.of(201, "방 생성 성공"));
+        }*/
+
+        return null;
+    }
+
+
 }
--- backend/src/main/java/com/ssafy/api/request/ProductsRegisterPostReq.java
+++ backend/src/main/java/com/ssafy/api/request/ProductsRegisterPostReq.java
@@ -1,2 +1,16 @@
-package com.ssafy.api.request;public class ProductsRegisterPostReq {
+package com.ssafy.api.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class ProductsRegisterPostReq {
+    private Long liveId;
+    private Long sellerId;
+    private String name;
+    private int leftTopX;
+    private int leftTopY;
+    private int rightBottomX;
+    private int rightBottomY;
 }
