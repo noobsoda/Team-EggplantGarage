72d33ca00a7398735b0571c0f1d28e5c2e0efdb9
doheeeeeeeee <jn96103@naver.com>
2023-02-03 12:02:15 +0900

    [Feat] Loaction

--- backend/src/main/java/com/ssafy/db/repository/AddressRepository.java
+++ backend/src/main/java/com/ssafy/db/repository/AddressRepository.java
@@ -2,11 +2,13 @@ package com.ssafy.db.repository;
 
 import com.ssafy.db.entity.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface AddressRepository extends JpaRepository<Location, Integer> {
-    Optional<List<Location>> findByLat(long lat);
-    Optional<List<Location>> findByLng(long lng);
-}
+//@Repository
+//public interface AddressRepository extends JpaRepository<Location, Long> {
+////    Optional<List<Location>> findByLat(long lat);
+////    Optional<List<Location>> findByLng(long lng);
+//}
