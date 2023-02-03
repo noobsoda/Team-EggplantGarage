90b9b14cf7f6042b71d66605e236f7bcb68d4e13
graywhale <bomerj@naver.com>
2023-02-03 14:44:23 +0900

    [Refactor] Organisms/Body 합치기

--- frontend/src/Pages/Main/Main.jsx
+++ frontend/src/Pages/Main/Main.jsx
@@ -1,14 +1,48 @@
 import React from "react";
-import MainBody from "../../Organisms/Body/MainBody";
 import Header from "../../Templates/Layout/Header";
+import Body from "../../Templates/Layout/Body";
 import Page from "../../Templates/Layout/Page";
+import styled from "styled-components";
+import LiveshowItem from "../../Molecules/Cards/LiveshowItem";
+import LiveStartBtn from "../../Atoms/IconButtons/LiveStartBtn";
+import CategoryNav from "../../Molecules/Category/CategoryNav";
+import { useNavigate } from "react-router-dom";
+
+const FlexDiv = styled.div`
+  width: 280px;
+  display: flex;
+  justify-content: space-between;
+`;
 
 export default function Main() {
+  const navigate = useNavigate();
+  function startLive() {
+    navigate("/liveshowsubmit");
+  }
   return (
     <Page>
       {/* 헤더 */}
       <Header isLogo="True" />
-      <MainBody />
+      <Body>
+        <div className="page-header" style={{ marginBottom: "8px" }}>
+          지역 Liveshow
+        </div>
+        <FlexDiv>
+          <LiveshowItem isViewer={true} />
+          <LiveshowItem isViewer={true} />
+          <LiveshowItem isViewer={true} />
+        </FlexDiv>
+        <div className="page-header" style={{ marginBottom: "8px" }}>
+          전국 Liveshow
+        </div>
+        <CategoryNav />
+        <FlexDiv>
+          <LiveshowItem isViewer={true} />
+          <LiveshowItem isViewer={true} />
+          <LiveshowItem isViewer={true} />
+        </FlexDiv>
+        <LiveStartBtn buttonClick={startLive} />
+      </Body>
     </Page>
   );
 }
