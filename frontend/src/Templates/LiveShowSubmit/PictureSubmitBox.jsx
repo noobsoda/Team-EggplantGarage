1437e7775f93d9d567695756bbf38fc16a86897e
deokisys <sjb378@me.com>
2023-02-05 17:07:18 +0900

    [Design] 카메라 키기 디자인 수정

--- frontend/src/Templates/LiveShowSubmit/PictureSubmitBox.jsx
+++ frontend/src/Templates/LiveShowSubmit/PictureSubmitBox.jsx
@@ -3,32 +3,46 @@ import styled from "styled-components";
 import Button from "../../Atoms/Buttons/ExtraSmallBtn";
 
 const StyledBox = styled.div`
-  width: 360px;
   height: 640px;
+  width: 280px;
+  margin: 24px 40px;
 `;
 const StyledGreenBox = styled.div`
   color: ${({ theme }) => theme.color.green};
+  padding-bottom: 56px;
+`;
+
+const StyledHead = styled.h1`
+  padding-bottom: 56px;
 `;
 
 const StyledImg = styled.img`
-  width: 360px;
+  width: 280px;
+  margin-top: 16px;
+`;
+
+const Space = styled.div`
+  height: ;
 `;
 
 export default function PictureSubmitBox({ imgSrc, cameraEvent }) {
   return (
     <StyledBox>
-      <h1 className="page-header">물품 등록</h1>
+      <StyledHead className="page-header">물품 등록</StyledHead>
 
       <StyledGreenBox className="body2-bold">
-        <p>라이브쇼에서 판매할 물건들을 모아서 찍어주세요.</p>
-        <p>사진 내 물품을 선택 후 물품 등록이 시작됩니다.</p>
+        <p>
+          라이브쇼에서 판매할 물건들을 모아서 찍어주세요.
+          <br />
+          <br />
+          사진 내 물품을 선택 후 물품 등록이시작됩니다.
+        </p>
       </StyledGreenBox>
 
       <h2 className="body1-header">사진 등록</h2>
 
       <div>
         <Button name="카메라" buttonClick={() => cameraEvent(true)} />
-        <Button name="앨범" />
       </div>
       <StyledImg src={imgSrc} alt="" />
     </StyledBox>
