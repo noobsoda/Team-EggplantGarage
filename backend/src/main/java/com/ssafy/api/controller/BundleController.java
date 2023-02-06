c0cdba581c06132b83d9dd85dcd935ce538fb267
장별하 <daisy1080@naver.com>
2023-02-06 16:24:37 +0900

    [Feat] 묶음 상품 구현 수정

--- backend/src/main/java/com/ssafy/api/controller/BundleController.java
+++ backend/src/main/java/com/ssafy/api/controller/BundleController.java
@@ -3,10 +3,12 @@ package com.ssafy.api.controller;
 import com.ssafy.api.request.BundleReq;
 import com.ssafy.api.service.BundleService;
 import com.ssafy.db.entity.Bundle;
+import com.ssafy.db.entity.Product;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import retrofit2.http.Path;
 
 import javax.transaction.Transactional;
 import java.util.List;
@@ -25,21 +27,42 @@ public class BundleController {
         Long bundleId = bundleService.addBundle(bundleReq);
         System.out.println("bundleId: " + bundleId);
         
-        // bundleId 프론트한테 보내줘야함
+        // 프론트한테 bundleId 보내주기
         return ResponseEntity.status(200).body("묶음 상품 등록 성공: " + bundleId);
     }
 
+    // 판매자 - 묶음 제안 목록
     @GetMapping("/{liveId}")
-    public ResponseEntity<?> getSuggestList(@PathVariable("liveId") Long liveId) {
-        List<Bundle> suggestList = bundleService.getSuggestList(liveId);
+    public ResponseEntity<?> getSellerSuggestList(@PathVariable("liveId") Long liveId) {
+        List<List<Product>> suggestList = bundleService.getSellerSuggestList(liveId);
         return ResponseEntity.status(200).body(suggestList);
     }
 
-    // 거부
-    @Transactional
-    @DeleteMapping("/{bundleId}")
+    // 구매자 - 묶음 제안 목록
+    @GetMapping("/{liveId}/{buyerId}")
+    public ResponseEntity<?> getBuyerSuggestList(@PathVariable("liveId") Long liveId, @PathVariable("buyerId") Long buyerId) {
+        List<List<Product>> getProductList = bundleService.getBuyerSuggestList(liveId, buyerId);
+        return ResponseEntity.status(200).body(getProductList);
+    }
+
+    // bundleId의 묶음 상품들 보기
+    @GetMapping("/items/{bundleId}")
+    public ResponseEntity<?> getBundledItemsRelation(@PathVariable("bundleId") Long bundleId) {
+        List<Product> getProductList = bundleService.getBundleItemsList(bundleId);
+        return ResponseEntity.status(200).body(getProductList);
+    }
+
+    // 승인
+    @PutMapping("/approval/{bundleId}")
+    public ResponseEntity<?> approvalBundle(@PathVariable("bundleId") Long bundleId) {
+        bundleService.approvalBundle(bundleId);
+        return ResponseEntity.status(200).body("승인");
+    }
+
+    // 거부 및 취소
+    @PutMapping("/refuse/{bundleId}")
     public ResponseEntity<?> refuseBundle(@PathVariable("bundleId") Long bundleId) {
-        bundleService.deleteBundle(bundleId);
-        return ResponseEntity.status(200).body("삭제 완료");
+        bundleService.refuseBundle(bundleId);
+        return ResponseEntity.status(200).body("거부 및 취소");
     }
 }
--- backend/src/main/java/com/ssafy/api/service/BundleService.java
+++ backend/src/main/java/com/ssafy/api/service/BundleService.java
@@ -2,11 +2,15 @@ package com.ssafy.api.service;
 
 import com.ssafy.api.request.BundleReq;
 import com.ssafy.db.entity.Bundle;
+import com.ssafy.db.entity.Product;
 
 import java.util.List;
 
 public interface BundleService {
     Long addBundle(BundleReq bundleReq);
-    List<Bundle> getSuggestList(Long liveId);
-    void deleteBundle(long bundleId);
+    List<Product> getBundleItemsList(long bundleId);
+    List<List<Product>> getSellerSuggestList(Long liveId);
+    List<List<Product>> getBuyerSuggestList(long liveId, long buyerId);
+    void approvalBundle(long bundleId);
+    void refuseBundle(long bundleId);
 }
