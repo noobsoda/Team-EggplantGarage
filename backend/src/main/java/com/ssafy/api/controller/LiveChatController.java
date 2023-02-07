d0f343b52f348c0e50c67a7edea80620d9444c01
장별하 <daisy1080@naver.com>
2023-02-07 16:40:54 +0900

    [Feat] 라이브채팅 config 수정

--- backend/src/main/java/com/ssafy/api/controller/LiveChatController.java
+++ backend/src/main/java/com/ssafy/api/controller/LiveChatController.java
@@ -5,14 +5,13 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 // @MessageMapping에 정의된 엔드포인트로 접근된 메시징 요청은 내부 처리 후
 // @SendTo 어노테이션을 통해 정의된 "/topic/public" 대상의 모든 가입자들에게 전송된다.
 @Controller
 //@RequestMapping("/chat")
-public class ChatController {
+public class LiveChatController {
     // @MessageMapping: 클라이언트에서 보내는 메시지를 매핑함
     // 호출되는 주소: /app/chat/sendMessage, /app/chat/addUser
     @MessageMapping("/sendMessage/{roomId}")
