5c5fca1ac9e1d8637e05d4b48cbc0adad27ceecd
서준배 <sjb378@me.com>
2023-02-08 09:06:23 +0900

    [Fix] useEffect추가

--- frontend/src/Pages/LiveShow/LiveShowSeller.jsx
+++ frontend/src/Pages/LiveShow/LiveShowSeller.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styled from "styled-components";
 import { useNavigate, useParams } from "react-router-dom";
 
