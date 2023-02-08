b70baeca95ced7bae7f572515b13c60007787f52
허진선 <wlstjs2478@naver.com>
2023-02-09 00:01:51 +0900

    [Feat] 1:1 채팅 - 채팅방 소켓 연결 & 채팅 메시지 출력 구현

--- frontend/src/Pages/Chat/ChatRoom.jsx
+++ frontend/src/Pages/Chat/ChatRoom.jsx
@@ -4,11 +4,11 @@ import Header from "../../Templates/Layout/Header";
 import Page from "../../Templates/Layout/Page";
 import { getStompClient } from "../../store/socket";
 
-const ChattingMessageItem = ({ user, message, sendDate, toUser }) => {
-  const name = "나"; // 리덕스에서 현재 사용자 이름 가져오기
+const ChattingMessageItem = ({ senderName, message, sendDate }) => {
+  const myName = "나"; // 리덕스에서 현재 사용자 이름 가져오기
   return (
     <div>
-      {user === name ? ( // 보낸 메시지
+      {senderName === myName ? ( // 보낸 메시지
         <div>
           <span>{sendDate}</span>
           <span>{message}</span>
@@ -17,7 +17,7 @@ const ChattingMessageItem = ({ user, message, sendDate, toUser }) => {
         // 받은 메시지
         <div>
           <div>
-            <span>{user}</span>
+            <span>{senderName}</span>
             <span>{message}</span>
             <span>{sendDate}</span>
           </div>
@@ -27,7 +27,7 @@ const ChattingMessageItem = ({ user, message, sendDate, toUser }) => {
   );
 };
 
-const ChattingMessage = ({ chattingMessages, toUser }) => {
+const ChattingMessage = ({ chattingMessages }) => {
   return (
     // 채팅 메시지가 존재하면 채팅 말풍선 생성
     <div>
@@ -35,11 +35,10 @@ const ChattingMessage = ({ chattingMessages, toUser }) => {
         chattingMessages.map((chattingMessage, i) => (
           <ChattingMessageItem
             key={i}
-            id={chattingMessage.id}
-            message={chattingMessage.message}
-            user={chattingMessage.user.name}
+            // id={chattingMessage.id}
+            message={chattingMessage.content}
+            senderName={"나"}
             sendDate={chattingMessage.sendDate}
-            toUser={toUser}
           />
         ))}
     </div>
@@ -48,26 +47,27 @@ const ChattingMessage = ({ chattingMessages, toUser }) => {
 
 export default function ChatRoom() {
   const roomId = useLocation().state.roomId; // 현재 URL을 통해 RoomId를 얻어옴
-  const [chattingMessages, setChattingMessages] = useState([]);
-  const [message, setMessage] = useState("");
-  const [userId, setUserId] = useState(1); // 리덕스로 초기값 가져오기로 수정
-  const [users, setUserList] = useState([]);
-  const [toUser, setToUser] = useState({});
+  const [chattingMessages, setChattingMessages] = useState([]); // 주고 받은 메시지 리스트
+  const [message, setMessage] = useState(""); // 입력창 메시지
+  const [userId, setUserId] = useState(1); // 나의 아이디(리덕스로 초기값 가져오기로 수정)
+  const [users, setUserList] = useState([]); // 유저 리스트(나, 상대방)
+  const [toUser, setToUser] = useState({}); // 상대방 유저
   const stompClient = getStompClient();
   const scrollRef = useRef();
   let navigate = useNavigate();
 
-  // 채팅 메시지 배열에 새로운 메시지 추가
+  // 메시지 배열에 새로운 메시지 추가
   const addMessage = (message) => {
     setChattingMessages((prev) => [...prev, message]);
   };
 
-  // stomp를 이용하여 메시지 전송
+  // 메시지 받기 : 받은 메시지를 메시지 배열에 추가
+  // stomp는 텍스트 처리만 가능하기 때문에 보낼 때 JSON.stringify(newMessage)) 받을 때 JSON.parse(data.body)처리를 꼭 해주어야 함
   const connect = () => {
     stompClient.connect(
       {},
       () => {
-        stompClient.subscribe("/sub/chat/room/" + roomId, (data) => {
+        stompClient.subscribe("/sub/room/" + roomId, (data) => {
           const newMessage = JSON.parse(data.body);
           addMessage(newMessage);
         });
@@ -85,41 +85,49 @@ export default function ChatRoom() {
       return;
     }
     connect();
-    // dispatch의 내부에 있는 chattingMessageList라는 함수의 액션 객체를 반환 받아 setChattingMessages, setUserList 하도록 로직 추가 필요 
-    setChattingMessages([]);
-    setUserList([]);
+    // dispatch의 내부에 있는 chattingMessageList라는 함수의 액션 객체를 반환 받아 setChattingMessages, setUserList 하도록 로직 추가 필요
+    setChattingMessages([]); // 일단 주고받은 메시지 없다고 선언
+    setUserList([
+      { id: 1, name: "나" },
+      { id: 2, name: "상대방" },
+    ]);
   }, []);
 
   // 의존성 변수 users가 변경될 때만 함수 호출
+  // 입장했을 때 setToUser를 설정하기 위해 실행됨
+  // sender가 상대방이라면 toUser(receiver)를 나로 설정
   useEffect(() => {
     users.map((user) => user.id != userId && setToUser(user));
   }, [users]);
 
   // 의존성 변수 chattingMessages가 변경될 때만 함수 호출
   // 새로운 메시지가 생성될때 채팅 스크롤
+  // 메시지가 추가될 경우 이벤트가 발생하여, 스크롤을 가장 밑으로 내림
   useEffect(() => {
     scrollRef.current.scrollIntoView({ behavior: "smooth", block: "end" });
   }, [chattingMessages]);
 
-  // Stomp를 이용하여 메시지 전송
+  // 메시지 보내기
   const sendMessage = () => {
     if (message != "") {
       stompClient.send(
-        "/pub/message",
+        "/pub/room/message",
         {},
         JSON.stringify({
-          message: message,
-          user: { id: userId },
-          chatRoom: { id: roomId },
+          content: message,
+          senderId: userId,
+          receiverId: toUser.id,
+          chatRoomId: roomId,
+          sendTime: null,
         })
       );
       scrollRef.current.scrollIntoView({ behavior: "smooth", block: "end" });
     }
-    setMessage("");
+    setMessage(""); // 메시지 전송 후 문자열 초기화
   };
 
   // Enter를 누르면 메시지 보냄
-  // 전송버튼 클릭시 전송도 추가 구현 필요
+  // 전송버튼 클릭시 전송도 추가 구현 필요!
   const onKeyPress = (e) => {
     if (e.key == "Enter") {
       sendMessage();
@@ -129,6 +137,28 @@ export default function ChatRoom() {
   return (
     <Page>
       <Header isName="True" headerName="채팅방" />
+      {/* scrollRef를 이용하여 아래 div 영역을 스크롤 조작 */}
+      <div ref={scrollRef}>
+        <div>
+          {users.map((user) => user.id != userId && <div>{toUser.name}</div>)}
+        </div>
+        <div>
+          <ChattingMessage chattingMessages={chattingMessages} />
+        </div>
+        <div>
+          <div>
+            <input
+              id="sendMessage"
+              type="text"
+              value={message}
+              placeholder="메시지를 입력하세요"
+              onChange={(e) => setMessage(e.target.value)}
+              onKeyPress={onKeyPress}
+            />
+          </div>
+          <button onClick={sendMessage}>전송</button>
+        </div>
+      </div>
     </Page>
   );
 }
