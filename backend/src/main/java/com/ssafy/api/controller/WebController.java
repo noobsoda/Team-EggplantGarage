5fcd65ebeea092b7553dcb2b7944b4e9b4c4cc88
yujaegyun <sodamito3@gmail.com>
2023-02-09 17:18:37 +0900

    [Fix] Router webcontroller 수정

--- backend/src/main/java/com/ssafy/api/controller/WebController.java
+++ backend/src/main/java/com/ssafy/api/controller/WebController.java
@@ -6,9 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WebController {
 
-    //리액트 url 라우팅 설정
-    @GetMapping(value = {"", "/home", "/liveshowsubmit", "/liveshowseller", "/login", "/signup", "/signupemail", "/search", "/like", "/chat", "/mypage", "category"
-    , "infoedit", "liveshowdetail", "writereview", "review"})
+    // 리액트 url 라우팅 설정
+    @GetMapping(value = { "", "/home", "/liveshowsubmit", "/liveshowseller", "/login", "/signup", "/signupemail",
+            "/search", "/like", "/chat", "/mypage", "/category", "/infoedit", "/liveshowdetail", "/writereview",
+            "/review",
+            "/liveshowbuyer" })
     public String forward() {
         return "forward:/index.html";
     }
