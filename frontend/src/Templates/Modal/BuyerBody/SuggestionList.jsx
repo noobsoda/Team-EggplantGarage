f596ec1db2d80737eb174bff63d18b202939d29b
서준배 <sjb378@me.com>
2023-02-10 17:49:25 +0900

    [Feat] 번들 취소 구현

--- frontend/src/Templates/Modal/BuyerBody/SuggestionList.jsx
+++ frontend/src/Templates/Modal/BuyerBody/SuggestionList.jsx
@@ -1,19 +1,40 @@
 import React, { useEffect } from "react";
 import BundleBox from "../../../Molecules/Cards/BundleBox";
 import ItemCard from "../../../Molecules/Cards/ItemCard";
+import { setBundleRefuse } from "../../../util/api/productApi";
 
 export default function SuggestionList({ isSeller, suggestList, getSuggest }) {
   useEffect(() => {
     getSuggest();
   }, []);
 
-  function bundleCancle() {}
-  function bundleAccept() {}
+  function bundleCancle(bundleId) {
+    console.log("번들 취소");
+    setBundleRefuse(
+      bundleId,
+      () => {
+        getSuggest();
+        console.log("취소 완료");
+      },
+      () => {
+        console.warn("bundle reject fail");
+      }
+    );
+  }
+  function bundleAccept(bundleId) {
+    console.log("해당 번들 수락");
+  }
   return (
     <>
       {suggestList.map((items, i) => {
+        console.log(items);
         return (
-          <BundleBox key={i} isSeller={isSeller}>
+          <BundleBox
+            key={i}
+            isSeller={isSeller}
+            reject={() => bundleCancle(items[0].bundleId)}
+            accept={() => bundleAccept(items[0].bundleId)}
+          >
             {items
               .map((ele) => {
                 return {
