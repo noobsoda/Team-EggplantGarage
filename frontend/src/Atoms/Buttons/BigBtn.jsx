72915e31410a306a73a17dc7170774f148fc8c67
허진선 <wlstjs2478@naver.com>
2023-02-12 16:26:46 +0900

    [Design] 로그인 페이지 CSS 수정

--- frontend/src/Atoms/Buttons/BigBtn.jsx
+++ frontend/src/Atoms/Buttons/BigBtn.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { Fragment } from "react";
 import styled from "styled-components";
 
-const StyledBigBtn = styled.button`
+const StyledBigBtn1 = styled.button`
   width: calc(100%);
   height: 40px;
   border-radius: 8px;
@@ -9,10 +9,56 @@ const StyledBigBtn = styled.button`
   color: ${({ theme }) => theme.color.white};
 `;
 
-export default function BigBtn({ name, buttonClick }) {
+const StyledDefaultBtn = styled.button`
+  display: block;
+  border-radius: 12px;
+  outline: none;
+  border: none;
+  background-image: linear-gradient(to right, #32be8f, #38d39f, #32be8f);
+  background-size: 200%;
+  // font-size: 1.2rem;
+  // font-family: "Poppins", sans-serif;
+  text-transform: uppercase;
+  //margin: 1rem 0;
+  cursor: pointer;
+  transition: 0.5s;
+  width: 100%;
+  height: 40px;
+  background-color: ${({ theme }) => theme.color.red};
+  color: ${({ theme }) => theme.color.white};
+`;
+
+const StyledWhiteBtn = styled.button`
+  display: block;
+  border-radius: 12px;
+  outline: none;
+  border: none;
+  background-size: 200%;
+  // font-size: 1.2rem;
+  // font-family: "Poppins", sans-serif;
+  text-transform: uppercase;
+  //margin: 1rem 0;
+  cursor: pointer;
+  transition: 0.5s;
+  width: 100%;
+  height: 40px;
+  background-color: ${({ theme }) => theme.color.lightgrey};
+  color: ${({ theme }) => theme.color.darkgrey};
+`;
+
+export default function BigBtn({ name, buttonClick, color }) {
   return (
-    <StyledBigBtn className="body1-header" onClick={buttonClick}>
-      {name}
-    </StyledBigBtn>
+    <Fragment>
+      {color === undefined && (
+        <StyledDefaultBtn className="body1-header" onClick={buttonClick}>
+          {name}
+        </StyledDefaultBtn>
+      )}
+      {color === "gray" && (
+        <StyledWhiteBtn className="body1-header" onClick={buttonClick}>
+          {name}
+        </StyledWhiteBtn>
+      )}
+    </Fragment>
   );
 }
--- frontend/src/Atoms/Inputs/BigInput.jsx
+++ frontend/src/Atoms/Inputs/BigInput.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import styled from "styled-components";
 
-const StyledInput = styled.input`
+const StyledInput1 = styled.input`
   width: calc(100% - 8px);
   height: 40px;
-
   border: 2px solid ${({ theme }) => theme.color.darkgrey};
   border-radius: 8px;
+  color: ${({ theme }) => theme.color.black};
+  text-align: ${(props) => props.textalign};
+`;
+
+const StyledInput = styled.input`
+  width: 100%;
+  position: relative;
+  font-size: 16px;
+  height: auto;
+  padding: 10px;
+  border-radius: 8px;
+  box-sizing: border-box;
+  transition: border-color ease-in-out 0.15s, box-shadow ease-in-out 0.15s;
+  border: 1px solid ${({ theme }) => theme.color.darkgrey};
   color: ${({ theme }) => theme.color.black};
   text-align: ${(props) => props.textalign};
 `;
