21fa85c87c6ce08bc456af1b4f1c622d39669de4
허진선 <wlstjs2478@naver.com>
2023-02-14 16:59:59 +0900

    [Design] 체크박스 색상 변경

--- frontend/src/Molecules/Cards/ItemCard.jsx
+++ frontend/src/Molecules/Cards/ItemCard.jsx
@@ -37,7 +37,7 @@ const Mask = styled.div`
   border-radius: 8px;
   align-items: center;
   background-color: rgb(10, 10, 10, 0.7);
-  color: red;
+  color: #BE2E22;
 `;
 const ItemImage = styled.div`
   border-radius: 10px 0px 0px 10px;
