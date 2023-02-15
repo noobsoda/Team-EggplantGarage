c3ffd05c20626422a58a58627a37c018945c569d
graywhale <bomerj@naver.com>
2023-02-15 11:58:34 +0900

    [Design] 라이브쇼 인포 메세지 css수정

--- frontend/src/Atoms/Text/MessageLive.jsx
+++ frontend/src/Atoms/Text/MessageLive.jsx
@@ -1,8 +1,32 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const Text = styled.span`
-  color: ${(props) => props.color};
+  /* color: ${(props) => props.color}; */
+  font-size: 14px;
+  ${(props) => {
+    if (props.color === "purple") {
+      return css`
+        color: ${({ theme }) => theme.color.purple};
+        font-weight: 500;
+        text-shadow: 1px 1px 2px purple;
+      `;
+    }
+    if (props.color === "green") {
+      return css`
+        color: ${({ theme }) => theme.color.green};
+        text-shadow: 1px 1px 2px green;
+        font-weight: 700;
+      `;
+    }
+    if (props.color === "red") {
+      return css`
+        color: #f92c33;
+        font-weight: 700;
+        text-shadow: 1px 1px 2px red;
+      `;
+    }
+  }}
 `;
 
 export default function MessageLive({ message }) {
