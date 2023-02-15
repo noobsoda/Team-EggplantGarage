46044abe58bd29bec2ac5217e8419af3204fae5d
허진선 <wlstjs2478@naver.com>
2023-02-15 11:03:19 +0900

    [Design] 채팅 메시지 애니메이션 추가

--- frontend/src/Organisms/Chat/ChattingMessageItem.jsx
+++ frontend/src/Organisms/Chat/ChattingMessageItem.jsx
@@ -2,18 +2,19 @@ import React from "react";
 import styled from "styled-components";
 import { format, render, cancel, register } from "timeago.js";
 import koLocale from "timeago.js/lib/lang/ko";
+import { motion } from "framer-motion";
 register("ko", koLocale);
 
 const Message = styled.div`
   padding: 8px;
 `;
-const FlexBox1 = styled.div`
+const FlexBox1 = styled(motion.div)`
   display: flex;
   flex-direction: row;
   column-gap: 8px;
   justify-content: flex-end;
 `;
-const FlexBox2 = styled.div`
+const FlexBox2 = styled(motion.div)`
   display: flex;
   flex-direction: row;
   column-gap: 8px;
@@ -31,7 +32,7 @@ const ReceivedBox = styled.div`
   padding: 12px;
   border-radius: 8px;
   // border: solid 2px ${({ theme }) => theme.color.graypurple};
-  background-color: ${({ theme }) => theme.color.whitegray};;
+  background-color: ${({ theme }) => theme.color.whitegray}; ;
 `;
 const DateBox = styled.div`
   color: ${({ theme }) => theme.color.grey};
@@ -44,7 +45,7 @@ export default function ChattingMessageItem({ message, sendDate, isMine }) {
   return (
     <Message>
       {isMine ? ( // 보낸 메시지
-        <FlexBox1>
+        <FlexBox1 positionTransition initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, transition: { duration: 0.15 } }}>
           <DateBox className="body3-bold">{format(sendDate, "ko")}</DateBox>
           <SendBox>
             <div>{message}</div>
@@ -53,7 +54,7 @@ export default function ChattingMessageItem({ message, sendDate, isMine }) {
       ) : (
         // 받은 메시지
 
-        <FlexBox2>
+        <FlexBox2 positionTransition initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, transition: { duration: 0.15 } }}>
           <ReceivedBox>
             <div>{message}</div>
           </ReceivedBox>
